325231c1a14da8e6338fe53a8ac537b5cf7b1bf1
hilaltaufiq <hilaltaufiq6@gmail.com>
2022-09-08 11:18:46 +0700

    ndog1

---
+++ JavaScript/thiskey.js
@@ -0,0 +1,10 @@
+const Mango ={
+    color: 'Yellow',
+    price: 120,
+    weight: 120,
+    about: function(){
+        // return 'this mango price is '+this.price+' USD';
+        return `This mengoooo Color is ${this.color} Name`;
+    }
+}
+console.log(Mango.about());
